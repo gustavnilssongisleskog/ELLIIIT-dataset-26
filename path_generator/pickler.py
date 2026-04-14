ae92210d58505df0970f14294ebe58c2c3099b7f
Gustav Nilsson Gisleskog <gisleskoggustav@gmail.com>
2026-04-14 11:27:49 +0200

    Functions to save and load random walks

---
+++ path_generator/pickler.py
@@ -0,0 +1,20 @@
+import xarray as xr
+import pickle
+from path_generator.path_generator import format_walks
+
+
+def load_pickle(file_name: str, ds: xr.Dataset):
+    if not file_name.endswith(".pickle"):
+        file_name += ".pickle"
+    with open(file_name, "rb") as file:
+        walks = pickle.load(file)
+
+    walks_formatted = format_walks(walks, ds)
+    return walks_formatted
+
+
+def save_pickle(file_name: str, walks):
+    if not file_name.endswith(".pickle"):
+        file_name += ".pickle"
+    with open(file_name, "wb") as file:
+        pickle.dump(walks, file)
