0128246f304f1339625ebafeeb80aa6c265d2652
Gustav Nilsson Gisleskog <gisleskoggustav@gmail.com>
2026-04-16 11:20:25 +0200

    Option for formatting or not when loading a pickle

--- path_generator/pickler.py
+++ path_generator/pickler.py
@@ -3,11 +3,14 @@ import pickle
 from path_generator.path_generator import format_walks
 
 
-def load_pickle(file_name: str, ds: xr.Dataset):
+def load_pickle(file_name: str, ds: xr.Dataset, formatting: bool=True):
     if not file_name.endswith(".pickle"):
         file_name += ".pickle"
     with open(file_name, "rb") as file:
         walks = pickle.load(file)
+
+    if not formatting:
+        return walks
 
     walks_formatted = format_walks(walks, ds)
     return walks_formatted
